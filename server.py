fa872ebf9e4b969d4eb1bed946bbc60f124180b0
F1203 <fernandomira1203@gmail.com>
2025-06-23 17:11:22 -0300

    Add files via upload

--- server.py
+++ server.py
@@ -125,4 +125,4 @@ def descargar_excel():
         return f"Error al generar el archivo: {e}", 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
